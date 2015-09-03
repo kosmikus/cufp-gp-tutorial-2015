cd9e87934c86d5c5cab0feec415f5d980b9d163c
Andres Loeh <andres@well-typed.com>
2015-09-03 23:45:32 +0200

    NPs.

--- src/NP.hs
+++ src/NP.hs
@@ -8,3 +8,41 @@
 {-# LANGUAGE FlexibleContexts #-}
 module NP where
 
+import GHC.Exts (Constraint)
+
+-- data List a = Nil | Cons a (List a)
+
+{-
+data List :: * -> * where
+  Nil   ::  List a
+  Cons  ::  a -> List a -> List a
+
+data HList :: [*] -> * where
+  HNil  ::  HList '[]
+  HCons ::  x -> HList xs -> HList (x ': xs)
+
+infixr 5 `HCons`
+-}
+
+data NP :: (k -> *) -> [k] -> * where
+  Nil  ::  NP f '[]
+  (:*) ::  f x -> NP f xs -> NP f (x ': xs)
+
+-- All :: (* -> Constraint) -> [*] -> Constraint
+
+type family All (c :: k -> Constraint) (xs :: [k]) :: Constraint where
+  All c '[]       = ()
+  All c (x ': xs) = (c x, All c xs)
+
+class (f (g x)) => Compose f g x
+instance (f (g x)) => Compose f g x
+
+deriving instance All (Compose Show f) xs => Show (NP f xs)
+
+infixr 5 :*
+
+newtype I a = I a
+  deriving Show
+
+newtype K a b = K a
+  deriving Show
