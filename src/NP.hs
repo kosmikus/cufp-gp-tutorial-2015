103ed712f9bf51a4da29929347303e661c1b1905
Andres Loeh <andres@well-typed.com>
2015-09-04 01:09:47 +0200

    End of NPs.

--- src/NP.hs
+++ src/NP.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 module NP where
 
+import Data.Proxy
 import GHC.Exts (Constraint)
 
 -- data List a = Nil | Cons a (List a)
@@ -46,3 +47,85 @@ newtype I a = I a
 
 newtype K a b = K a
   deriving Show
+
+map_NP :: (forall x . f x -> g x) -> NP f xs -> NP g xs
+map_NP _ Nil       = Nil
+map_NP f (x :* xs) = f x :* map_NP f xs
+
+eq_NP :: (All Eq xs) => NP I xs -> NP I xs -> Bool
+eq_NP Nil         Nil         = True
+eq_NP (I x :* xs) (I y :* ys) = x == y && eq_NP xs ys
+eq_NP _           _           = error "impossible"
+
+cmap_NP :: (All c xs) => Proxy c -> (forall x . c x => f x -> g x) -> NP f xs -> NP g xs
+cmap_NP _ _ Nil       = Nil
+cmap_NP p f (x :* xs) = f x :* cmap_NP p f xs
+
+
+-- pure  :: a -> f a
+
+class Pure_NP (xs :: [k]) where
+  pure_NP' :: (forall x . f x) -> NP f xs
+
+instance Pure_NP '[] where
+  pure_NP' x = Nil
+
+instance (Pure_NP xs) => Pure_NP (x ': xs) where
+  pure_NP' x = x :* pure_NP' x
+
+class SingI (xs :: [k]) where
+  sing :: Sing xs
+
+instance SingI '[] where
+  sing = SNil
+
+instance (SingI xs) => SingI (x ': xs) where
+  sing = SCons
+
+data Sing :: [k] -> * where
+  SNil  :: Sing '[]
+  SCons :: SingI xs => Sing (x ': xs)
+
+pure_NP :: SingI xs => (forall x . f x) -> NP f xs
+pure_NP x = go sing x
+  where
+    go :: Sing xs -> (forall x . f x) -> NP f xs
+    go SNil  _ = Nil
+    go SCons y = y :* pure_NP y
+
+cpure_NP :: (SingI xs, All c xs) => Proxy c -> (forall x . c x => f x) -> NP f xs
+cpure_NP = undefined
+
+-- (<*>) :: f (a -> b) -> f a -> f b
+
+ap_NP :: NP (f -.-> g) xs -> NP f xs -> NP g xs
+ap_NP Nil          Nil       = Nil
+ap_NP (Fn f :* fs) (x :* xs) = f x :* ap_NP fs xs
+ap_NP _            _         = error "impossible"
+
+newtype (f -.-> g) x = Fn (f x -> g x)
+
+map_NP' :: SingI xs => (forall x . f x -> g x) -> NP f xs -> NP g xs
+map_NP' f xs = pure_NP (Fn f) `ap_NP` xs
+
+zipWith_NP :: SingI xs => (forall x . f x -> g x -> h x) -> NP f xs -> NP g xs -> NP h xs
+zipWith_NP f xs ys = pure_NP (fn_2 f) `ap_NP` xs `ap_NP` ys
+
+czipWith_NP :: (All c xs, SingI xs)
+            => Proxy c
+            -> (forall x . c x => f x -> g x -> h x)
+            -> NP f xs -> NP g xs -> NP h xs
+czipWith_NP p f xs ys = cpure_NP p (fn_2 f) `ap_NP` xs `ap_NP` ys
+
+fn_2 f = Fn (\ x -> Fn (\ y -> f x y))
+
+
+eq_NP' :: (All Eq xs, SingI xs) => NP I xs -> NP I xs -> Bool
+eq_NP' xs ys =
+    and
+  $ collapse_NP
+  $ czipWith_NP (Proxy :: Proxy Eq) (\ (I x) (I y) -> K (x == y)) xs ys
+
+collapse_NP :: NP (K a) xs -> [a]
+collapse_NP Nil         = []
+collapse_NP (K x :* xs) = x : collapse_NP xs
