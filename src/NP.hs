d3cde3448a260dd728213b8f5d4b0913340e3316
Andres Loeh <andres@well-typed.com>
2015-09-03 22:21:43 +0200

    Add an empty file with just the language extensions.

---
+++ src/NP.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE GADTs, KindSignatures, DataKinds, PolyKinds, TypeOperators #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, UndecidableInstances, FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+module NP where
+
